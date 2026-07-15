d66f4b1fbbe4cc5772bf3fbdbf4bf3720d5bbfc1
Nescript <raymond059@qq.com>
2026-07-15 15:05:44 +0800

    Add a rust like Dump method in ast.hpp

--- include/ast.hpp
+++ include/ast.hpp
@@ -6,14 +6,20 @@
 #include <ostream>
 #include <string>
 
+inline void print_indent(std::ostream& os, int indent) {
+  for (int i = 0; i < indent; ++i) {
+    os << "  ";
+  }
+}
+
 class BaseAST {
 public:
   virtual ~BaseAST() = default;
-  virtual void Dump(std::ostream& os) const = 0;
+  virtual void Dump(std::ostream& os, int indent) const = 0;
 };
 
 inline std::ostream& operator<<(std::ostream& os, const std::unique_ptr<BaseAST>& ast) {
-  if (ast) ast->Dump(os);
+  if (ast) ast->Dump(os, 0);
   else os << "nullptr";
   return os;
 }
@@ -21,8 +27,12 @@ inline std::ostream& operator<<(std::ostream& os, const std::unique_ptr<BaseAST>
 class CompUnitAST : public BaseAST {
 public:
   std::unique_ptr<BaseAST> func_def;
-  void Dump(std::ostream& os) const override {
-    os << "CompUnitAST { " << func_def << " }";
+  void Dump(std::ostream& os, int indent) const override {
+    print_indent(os, indent);
+    os << "CompUnitAST { " << std::endl;
+    func_def->Dump(os, indent + 1);
+    print_indent(os, indent);
+    os << "}" << std::endl;
   }
 };
 
@@ -31,24 +41,40 @@ public:
   std::unique_ptr<BaseAST> func_type;
   std::string ident;
   std::unique_ptr<BaseAST> block;
-  void Dump(std::ostream& os) const override {
-    os << "FuncDefAST { " << func_type << ", " << ident << ", " << block << " }";
+  void Dump(std::ostream& os, int indent) const override {
+    print_indent(os, indent);
+    os << "FuncDefAST { " << std::endl;
+    func_type->Dump(os, indent + 1);
+    print_indent(os, indent + 1);
+    os << "ident: " << ident << std::endl;
+    block->Dump(os, indent + 1);
+    print_indent(os, indent);
+    os << "}" << std::endl;
   }
 };
 
 class FuncTypeAST : public BaseAST {
 public:
   std::unique_ptr<std::string> type;
-  void Dump(std::ostream& os) const override {
-    os << "FuncTypeAST { " << *type << " }";
+  void Dump(std::ostream& os, int indent) const override {
+    print_indent(os, indent);
+    os << "FuncTypeAST { " << std::endl;
+    print_indent(os, indent + 1);
+    os << "type: " << *type << std::endl;
+    print_indent(os, indent);
+    os << "}" << std::endl;
   }
 };
 
 class BlockAST : public BaseAST {
 public:
   std::unique_ptr<BaseAST> stmt;
-  void Dump(std::ostream& os) const override {
-    os << "BlockAST { " << stmt << " }";
+  void Dump(std::ostream& os, int indent) const override {
+    print_indent(os, indent);
+    os << "BlockAST { " << std::endl;
+    stmt->Dump(os, indent + 1);
+    print_indent(os, indent);
+    os << "}" << std::endl;
   }
 };
 
@@ -56,7 +82,12 @@ class StmtAST : public BaseAST {
 public:
   int number;
   // 这里是int还是别的longlong呢？要取决于语言的定义
-  void Dump(std::ostream& os) const override {
-    os << "StmtAST { " << number << " }";
+  void Dump(std::ostream& os, int indent) const override {
+    print_indent(os, indent);
+    os << "StmtAST { " << std::endl;
+    print_indent(os, indent + 1);
+    os << "number: " << number << std::endl;
+    print_indent(os, indent);
+    os << "}" << std::endl;
   }
 };
